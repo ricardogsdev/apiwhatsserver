de332eab05c95a95ceb1dd1f9da1e95e941f64ac
Ricardo Gomes <ricardogsdev@gmail.com>
2025-06-07 05:27:22 -0300

    ajustes

--- app.js
+++ app.js
@@ -20,6 +20,13 @@ app.use(cors({
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'apitoken', 'sessionkey', 'session', 'Authorization', 'x-csrf-token', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Methods', 'Access-Control-Allow-Headers'],
 }));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", req.headers.origin || "https://optovision.com.br");
+    res.header("Access-Control-Allow-Methods", "GET, POST");
+    res.header("Access-Control-Allow-Headers", "Content-Type, apitoken, sessionkey");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
 
 require('dotenv').config();
 
